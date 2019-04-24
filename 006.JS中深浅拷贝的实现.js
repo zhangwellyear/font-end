1d5a8b6113d98aa8bdcfb1c693399458f48d9a92
zhangwellyeaer <zhangwellyear@163.com>
2019-04-24 19:39:02 +0800

    day1 JS深拷贝的实现

--- 006.JS中深浅拷贝的实现.js
+++ 006.JS中深浅拷贝的实现.js
@@ -16,76 +16,76 @@
 /**
  * 当数组中嵌套了对象或数组之类的引用类型数据时，结果就大不相同了
  */
-// var arr = [{old: 'old'}, ['old']];
-// var new_arr = arr.concat();
+var arr = [{old: 'old'}, ['old']];
+var new_arr = arr.concat();
 
-// arr[0].old = 'new';
-// arr[1][0] = 'new';
+arr[0].old = 'new';
+arr[1][0] = 'new';
 
-// console.log(arr);       // [{old: 'new'}, ['new']]
-// console.log(new_arr);   // [{old: 'new'}, ['new']]
+console.log(arr);       // [{old: 'new'}, ['new']]
+console.log(new_arr);   // [{old: 'new'}, ['new']]
 
 
 /**
  * 有个技巧可以深拷贝一个数组或对象
  */
-// var arr = [{old: 'old'}, ['old']];
-// var new_arr = JSON.parse(JSON.stringify(arr));
+var arr = [{old: 'old'}, ['old']];
+var new_arr = JSON.parse(JSON.stringify(arr));
 
-// arr[0].old = 'new';
-// arr[1][0] = 'new';
+arr[0].old = 'new';
+arr[1][0] = 'new';
 
-// console.log(arr);       // [ { old: 'new' }, [ 'new' ] ]
-// console.log(new_arr);   // [ { old: 'old' }, [ 'old' ] ]
+console.log(arr);       // [ { old: 'new' }, [ 'new' ] ]
+console.log(new_arr);   // [ { old: 'old' }, [ 'old' ] ]
 
 
 /**
  * 上面的深拷贝有个问题，无法拷贝函数
  */
-// var arr = [function () {
-//     console.log(a)
-// }, {
-//     b: function () {
-//         console.log(b);
-//     }
-// }]
+var arr = [function () {
+    console.log(a)
+}, {
+    b: function () {
+        console.log(b);
+    }
+}]
 
-// var new_arr = JSON.parse(JSON.stringify(arr));
-// console.log(new_arr);   // [ null, {} ], new_arr的结果并没有被完整的还原
+var new_arr = JSON.parse(JSON.stringify(arr));
+console.log(new_arr);   // [ null, {} ], new_arr的结果并没有被完整的还原
 
 
 /**
  * 浅拷贝的实现
  * 思路：很简单，遍历对象，然后把属性和属性值都放在一个新的对象
  */
-// var shallowCopy = function (obj) {
-//     // 只拷贝对象
-//     if (typeof obj !== 'object') return;
-//     // 判断对象是否为数组
-//     var new_obj = obj instanceof Array? [] : {};
-//     for (key in obj) {
-//         if (obj.hasOwnProperty(key)) {
-//             new_obj[key] = obj[key];
-//         }
-//     }
+var shallowCopy = function (obj) {
+    // 只拷贝对象
+    if (typeof obj !== 'object') return;
+    // 判断对象是否为数组
+    var new_obj = obj instanceof Array? [] : {};
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            new_obj[key] = obj[key];
+        }
+    }
 
-//     return new_obj
-// }
+    return new_obj
+}
 
-// var arr = [function () {
-//     console.log(a)
-// }, {
-//     b: function () {
-//         console.log(b);
-//     }
-// }];
+var arr = [function () {
+    console.log(a)
+}, {
+    b: function () {
+        console.log(b);
+    }
+}];
 
-// var new_arr = shallowCopy(arr);
-// console.log(new_arr);   // [ [Function], { b: [Function: b] } ]
+var new_arr = shallowCopy(arr);
+console.log(new_arr);   // [ [Function], { b: [Function: b] } ]
 
 
 /**
- * 深拷贝的实现
+ * 深拷贝的实现（深度优先遍历的方式）
  * 思路：判断一个对象的属性值的类型是否为对象，如果是对象，递归调用深度拷贝函数，实现深拷贝
  */
 var deepCopy = function (obj) {
